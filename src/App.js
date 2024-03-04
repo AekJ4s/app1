934a8c7e299fcb3b30a04112f64d3fb9e7dad9bc
AekJ4s <Jasdakornam@gmail.com>
2024-03-04 11:53:23 +0700

    Finish work

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import FormBootstrap from './form-bootstrap'
 import FormText from './form-text'
 import FormCheck from './form-check'
 import FormSelectFile from './form-select-file'
+import FormValidation from './form-validation'
 export default function App(){
-return <FormSelectFile/>
+return <FormValidation/>
 }
