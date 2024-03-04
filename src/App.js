597451e690fd2c3e24fcd8723d9fc7eda5e5b4c5
AekJ4s <Jasdakornam@gmail.com>
2024-03-04 10:20:54 +0700

    summary

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import FormSearch from "./form-search"
 import FormBootstrap from './form-bootstrap'
 import FormText from './form-text'
+import FormCheck from './form-check'
 export default function App(){
-return <FormText/>
+return <FormCheck/>
 }
