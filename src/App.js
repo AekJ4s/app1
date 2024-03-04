266b4e4440b76c2bd19d7aeca6b4b8f5f070bf4c
AekJ4s <Jasdakornam@gmail.com>
2024-03-04 11:05:53 +0700

    Create form-select-file

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import FormSearch from "./form-search"
 import FormBootstrap from './form-bootstrap'
 import FormText from './form-text'
 import FormCheck from './form-check'
+import FormSelectFile from './form-select-file'
 export default function App(){
-return <FormCheck/>
+return <FormSelectFile/>
 }
--- src/form-check.js
+++ src/form-check.js
@@ -38,7 +38,7 @@ export default function FormCheck() {
         alert(msg)
     }
 
-    return (<div className='mt-4 mx-auto p-3' style={{ width: '450px', background: '#cee' }}>
+    return (<div className='mt-4 mx-auto p-3 rounded' style={{ width: '450px', background: '#cee' }}>
 
         <form>
         <span>font style:</span> &nbsp;&nbsp;
